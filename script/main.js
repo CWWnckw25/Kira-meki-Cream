e9283e29c3aa8eca66eca17c061a9833acf6fc3c
CWWnckw25 <yitchikuwaling512@gmail.com>
2024-03-22 23:00:33 +0900

    1回目のコミット

---
+++ script/main.js
@@ -0,0 +1,9 @@
+$('.Voices__container--slider--content').slick({  
+    autoplay: true, //自動再生
+	autoplaySpeed: 5000, //自動再生のスピード
+	speed: 1500,//スライドするスピード
+    arrows: true, // 左右のナビゲーションを有効にする
+    dots: true, // ドットを表示
+    fade: false, // フェードで切り替えなし
+    infinite: true, //スライドのループ
+});
