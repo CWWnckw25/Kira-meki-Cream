072f71548345d0e18ea1ff50cc4981e024c7f51e
CWWnckw25 <yitchikuwaling512@gmail.com>
2024-03-23 21:17:47 +0900

    3回目のコミット

--- script/main.js
+++ script/main.js
@@ -1,9 +1,25 @@
-$('.voice__container--slider--content').slick({  
-    autoplay: true, //自動再生
-	autoplaySpeed: 5000, //自動再生のスピード
-	speed: 1500,//スライドするスピード
-    arrows: true, // 左右のナビゲーションを有効にする
-    dots: true, // ドットを表示
-    fade: false, // フェードで切り替えなし
-    infinite: true, //スライドのループ
-});
+//画面幅が768px以下の場合、スライダーの表示は1つになり、画面幅が768px以上の場合、スライダーの表示は3つになります。
+// Path: script/main.js
+$(window).on('load resize', function(){
+    if($(window).width() > 768){
+        $('.voice__container--slider--content').not('.slick-initialized').slick({  
+            autoplay: true, //自動再生
+            speed: 5000,//スライドするスピード
+            arrows: true, // 左右のナビゲーションを有効にする
+            dots: true, // ドットを表示
+            fade: false, // フェードで切り替えなし
+            infinite: true, //スライドのループ
+            slidesToShow: 3,
+            slidesToScroll: 3
+        });}
+    else{
+        $('.voice__container--slider--content').not('.slick-initialized').slick({  
+            autoplay: true, //自動再生
+            speed: 5000,//スライドするスピード
+            arrows: true, // 左右のナビゲーションを有効にする
+            dots: true, // ドットを表示
+            fade: false, // フェードで切り替えなし
+            infinite: true, //スライドのループ
+            slidesToShow: 1
+    });
+}});
