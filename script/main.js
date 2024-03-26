c21651a8b95639bc9fd2093b392c7944b1212245
CWWnckw25 <yitchikuwaling512@gmail.com>
2024-03-26 19:22:09 +0900

    6回目のコミット

--- script/main.js
+++ script/main.js
@@ -1,34 +1,26 @@
-//画面幅が768px以下の場合、スライダーの表示は1つになり、画面幅が768px以上の場合、スライダーの表示は3つになります。
 // Path: script/main.js
-$(window).on('load resize', function(){
-    if($(window).width() > 768){
-        $('.voice__container--slider--content').not('.slick-initialized').slick({  
-            autoplay: true, //自動再生
-            speed: 5000,//スライドするスピード
-            arrows: true, // 左右のナビゲーションを有効にする
-            dots: true, // ドットを表示
-            fade: false, // フェードで切り替えなし
-            infinite: true, //スライドのループ
-            slidesToShow: 3,
-            slidesToScroll: 3
-        });}
-    else{
-        $('.voice__container--slider--content').not('.slick-initialized').slick({  
-            autoplay: true, //自動再生
-            speed: 5000,//スライドするスピード
-            arrows: true, // 左右のナビゲーションを有効にする
-            dots: true, // ドットを表示
-            fade: false, // フェードで切り替えなし
-            infinite: true, //スライドのループ
-            slidesToShow: 1
+    $('.voice__container--slider--content').slick({  
+        autoplay: true, //自動再生
+        speed: 5000,//スライドするスピード
+        arrows: true, // 左右のナビゲーションを有効にする
+        dots: true, // ドットを表示
+        fade: false, // フェードで切り替えなし
+        infinite: true, //スライドのループ
+        slidesToShow: 3,
+        slidesToScroll: 3,
+
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            }
+        }]
     });
-}});
+
 
 // アコーディオンのタイトルがクリックされたら
 $('.accordion-title').on('click', function(e) {
-
-    // .accordion-contentを選択
-    var content = $(this).next();
-    // .accordion-contentを表示・非表示
-    content.slideToggle();
+    $(this).toggleClass('accordion-title--active');
+    $(this).next().slideToggle();
     });
