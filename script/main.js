ff367a525b1214a3d25350b030510884a52fb5f2
CWWnckw25 <yitchikuwaling512@gmail.com>
2024-03-29 17:46:24 +0900

    7回目のコミット

--- script/main.js
+++ script/main.js
@@ -23,9 +23,14 @@
 
 // アコーディオンのタイトルがクリックされたら
 $('.accordion-title').on('click', function(e) {
-    $(this).toggleClass('.accordion-title--active');
+    $(this).toggleClass('accordion-title--active');
     $(this).next().slideToggle();
     });
-    $('.accordion-title').on('click', function(e) {
-        $(this).toggleClass('.accordion-arrow-icon');
-    });
+
+    // #page-topをクリックした際の設定
+$('.price-arrow-up').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
