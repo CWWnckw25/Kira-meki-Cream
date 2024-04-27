1e785526d997f7e481f0f214fac5e7cf8d5d688c
CWWnckw25 <yitchikuwaling512@gmail.com>
2024-04-27 16:56:42 +0900

    15回目のコミット

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,35 +1,34 @@
-// Path: script/main.js
-    $('.voice__container--slider--content').slick({  
-        autoplay: true, //自動再生
-        speed: 5000,//スライドするスピード
-        arrows: true, // 左右のナビゲーションを有効にする
-        dots: true, // ドットを表示
-        fade: false, // フェードで切り替えなし
-        infinite: true, //スライドのループ
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        prevArrow: '<img src="/assets/images/arrow-left-circle-fill.png" class="slide-arrow prev-arrow" alt="">',
-        nextArrow: '<img src="/assets/images/arrow-right-circle-fill.png" class="slide-arrow next-arrow"alt="">',
+$('.voice__container--slider--content').slick({
+    autoplay: true,
+    speed: 5000,
+    arrows: true,
+    dots: true,
+    fade: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: '<img src="/assets/images/arrow-left-circle-fill.png" class="slide-arrow prev-arrow" alt="">',
+    nextArrow: '<img src="/assets/images/arrow-right-circle-fill.png" class="slide-arrow next-arrow"alt="">',
 
-        responsive: [{
-            breakpoint: 768,
-            settings: {
+    responsive: [{
+        breakpoint: 768,
+        settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
-            }
-        }]
-    });
+        }
+    }]
+});
 
 
 // アコーディオンのタイトルがクリックされたら
-$('.accordion-title').on('click', function(e) {
+$('.accordion-title').on('click', function (e) {
     $(this).toggleClass('accordion-title--active');
     $(this).next().slideToggle();
-    });
+});
 
-    
+
 //スクロールするとフェードイン
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(this).scrollTop() > $(window).height() / 10) {
         $('.price-arrow-up').fadeIn();
     } else {
